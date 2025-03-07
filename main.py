43a87d0a68a4a3d516de9ba5cf93c92ad2883591
〰️AI〰️ <aitom0301@gmail.com>
2025-03-08 03:25:36 +0500

    Add files via upload

--- main.py
+++ main.py
@@ -1,50 +1,23 @@
-import requests
+from bottle import route, run, request, template
 import logging
-from fastapi import FastAPI, HTTPException
-from bs4 import BeautifulSoup
-from typing import Dict
 
-from structuring_data.structuring_data import create_data
-from parser.parser import get_whois_data
-from create_table.create_table import create_table, save_to_db
+from whois import whois_data_check
 
-# Logging configuration
 logging.basicConfig(level=logging.INFO)
 
-app = FastAPI()
-create_table()
 
-@app.get("/lookup_whois")
-def lookup_whois(domain: str) -> Dict[str, str]:
-    logging.info(f"Received WHOIS lookup request for domain: {domain}")
+@route('/')
+def index():
+    return template('index')
 
-    url = f"https://www.ps.kz/domains/whois/result?q={domain}"
-    headers = {
-        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/113.0.0.0 Safari/537.36"
-    }
 
-    try:
-        response = requests.get(url, headers=headers)
-        response.raise_for_status()
-    except requests.RequestException as e:
-        logging.error(f"Error fetching WHOIS data for {domain}: {str(e)}")
-        raise HTTPException(status_code=500, detail=f"Error fetching WHOIS data: {str(e)}")
+@route('/result', method='POST')
+def result():
+    text = request.forms.get('text')
+    logging.info(f"Получен запрос на WHOIS для домена: {text}. Проверяем корректность доменного имени.")
 
-    soup = BeautifulSoup(response.text, 'html.parser')
-    raw_data = get_whois_data(soup)
+    whois_data = whois_data_check(text)
+    return template('result', result=whois_data)
 
-    whois_data = create_data(raw_data)
 
-    try:
-        save_to_db(whois_data)
-        logging.info(f"WHOIS data for {domain} successfully saved to the database")
-    except Exception as e:
-        logging.error(f"Error saving WHOIS data for {domain}: {str(e)}")
-        raise HTTPException(status_code=500, detail=f"Error saving data to the database: {str(e)}")
-
-    return whois_data
-
-if __name__ == "__main__":
-    import uvicorn
-    logging.info("Starting FastAPI server...")
-    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
+run(host='localhost', port=8080, debug=True)
