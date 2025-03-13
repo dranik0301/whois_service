ed4eeba63e6a9a8e197f463df99886843ba81418
〰️AI〰️ <aitom0301@gmail.com>
2025-03-13 23:06:44 +0500

    Update whois.py

--- whois_service_cURL/services/whois.py
+++ whois_service_cURL/services/whois.py
@@ -19,7 +19,6 @@ HEADERS = {
     'origin': 'https://www.ps.kz',
     'referer': 'https://www.ps.kz/',
     'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/134.0.0.0 Safari/537.36',
-
 }
 
 API_URL = "https://console.ps.kz/vitrina/graphql"
